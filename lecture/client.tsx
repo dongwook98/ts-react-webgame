4fe47b2b9f1a5bb2d234eec1226908657bf9acbc
dongwook98 <ehddnr8563@gmail.com>
2023-11-17 07:17:16 +0900

    fix(error): createRoot does not exist 문제 해결

---
+++ lecture/client.tsx
@@ -0,0 +1,8 @@
+// import React = require('react');
+// import ReactDOM = require('react-dom');
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+
+import GuGuDan from './GuGuDan';
+
+ReactDOM.createRoot(document.querySelector('#root')!).render(<GuGuDan />);
