7f13687d4fd8732ee863d4ad47f51ff5841e1a02
dongwook98 <ehddnr8563@gmail.com>
2023-11-17 07:14:35 +0900

    feat(component): 구구단 컴포넌트 추가

---
+++ lecture/GuGuDan.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { useState, useRef } from 'react';
+
+const GuGuDan = () => {
+  const [first, setFirst] = useState(Math.ceil(Math.random() * 9));
+  const [second, setSecond] = useState(Math.ceil(Math.random() * 9));
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const onSubmitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    // 분리하면 타이핑해줘야함
+    e.preventDefault();
+    const input = inputRef.current;
+    if (parseInt(value) === first * second) {
+      setResult('정답');
+      setFirst(Math.ceil(Math.random() * 9));
+      setSecond(Math.ceil(Math.random() * 9));
+      setValue('');
+      input!.focus();
+    } else {
+      setResult('땡');
+      setValue('');
+      input!.focus();
+    }
+  };
+  return (
+    <>
+      <div>
+        {first} 곱하기 {second}는?
+      </div>
+      <form onSubmit={onSubmitForm}>
+        <input ref={inputRef} type='number' value={value} onChange={(e) => setValue(e.target.value)} />
+      </form>
+    </>
+  );
+};
+
+export default GuGuDan;
